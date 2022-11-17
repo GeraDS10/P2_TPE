b9ccc2d4179bf1e196c338806aa09674d9f474c0
GeraDS10 <gerardosaldain10@gmail.com>
2022-11-17 14:28:36 -0300

    Punto 1 casi listo, falta orenamiento

--- ListaVinculadaOrdenada.java
+++ ListaVinculadaOrdenada.java
@@ -6,11 +6,84 @@ public class ListaVinculadaOrdenada {
     public ListaVinculadaOrdenada(){
 
     }
+    public void setOrdenador(Ordenador o){
+        this.ordenador = o;
+        ordenarLista();
+    }
+    private void ordenarLista(){
+        Nodo anterior = this.primero;
+        Nodo iterador= this.primero;
+        while(iterador.getSiguiente() != null){
+            if(this.ordenador.esMenor(anterior, iterador)){
+                anterior = iterador;
+                iterador = iterador.getSiguiente();
+            }
+            else{
+                anterior.setSiguiente(iterador.getSiguiente());
+                //seguir desde aca
+            }
+        }
+    }
+
+    public void imprimirLista(){
+        int posicionActual = 0;
+        Nodo iterador = this.primero;
+        while(iterador.getSiguiente() != null){
+            System.out.println(iterador.getObjeto() + " en posicion " + posicionActual);
+            iterador = iterador.getSiguiente();
+            posicionActual++;
+        }
+    }
+
+    public void obtenerPosicion(Object o){
+        int posicionActual = 0;
+        Nodo anterior = this.primero;
+        Nodo iterador = this.primero;
+        while ((iterador.getSiguiente() != null) && (!iterador.getObjeto().equals(o))){
+            anterior = iterador;
+            iterador = iterador.getSiguiente();
+            posicionActual++;
+        }
+        if(iterador.getSiguiente() != null){
+            System.out.println("No se encontro objeto");
+        }
+        else{
+            System.out.println("Objeto encontrado en posicion: " + posicionActual);
+        }
+    }
+    public void eliminarPosicion(int pos){
+        int posicionActual = 0;
+        Nodo anterior = this.primero;
+        Nodo iterador = this.primero;
+        while((pos != posicionActual) && (iterador.getSiguiente() != null)){
+            anterior = iterador;
+            iterador = iterador.getSiguiente();
+        }
+        if(iterador.getSiguiente() == null){
+            System.out.println("No existe objeto en la posicion ingresada");
+        }
+        else{
+            anterior.setSiguiente(iterador.getSiguiente());
+        }
+    }
 
 
+    public void eliminarObjeto(Object o){
+        Nodo anterior = this.primero;
+        Nodo iterador = this.primero;
+        while (iterador.getSiguiente() != null){
+            if(iterador.getObjeto().equals(o)){
+                anterior.setSiguiente(iterador.getSiguiente());
+            }
+            else{
+                anterior = iterador;
+            }
+            iterador = iterador.getSiguiente();
+
+        }
+    }
     public void insertarObjeto(Object o){
-        Nodo nuevo = new Nodo();
-        nuevo.insertarObjeto(o);
+        Nodo nuevo = new Nodo(o);
         if (this.primero == null){
             this.primero = nuevo;
         }
@@ -33,7 +106,6 @@ public class ListaVinculadaOrdenada {
                     nuevo.setSiguiente(adelante);
                     atras.setSiguiente(nuevo);
                 }
-
             }
         }
     }
--- Nodo.java
+++ Nodo.java
@@ -1,11 +1,20 @@
 public class Nodo {
 
-    private Object objeto = null;
+    private Object objeto;
     private Nodo siguiente;
+
+    public Nodo(Object o){
+        this.objeto = o;
+        this.siguiente = null;
+    }
+
+    /*
 
     public void insertarObjeto(Object o){
         this.objeto = o;
     }
+
+     */
 
     public void setSiguiente(Nodo sig){
         this.siguiente = sig;
